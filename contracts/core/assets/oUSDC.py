cebae6c4ea95b9363a26cd668d2286e5ccceb8e0
tanyuan <1067598718@qq.com>
2020-09-14 11:38:50 +0800

    rename

--- contracts/core/assets/oUSDC.py
+++ contracts/core/assets/oUSDC.py
@@ -14,8 +14,8 @@ TransferOwnershipEvent = RegisterAction("transferOwnership", "oldOwner", "newOwn
 
 ctx = GetContext()
 
-NAME = 'oUSDC'
-SYMBOL = 'oUSDC'
+NAME = 'pUSDC'
+SYMBOL = 'pUSDC'
 DECIMALS = 6
 FACTOR = 1000000
 Operator = Base58ToAddress("AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p")
